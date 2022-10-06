1e3bb65817fa3cd0a2fc253c14a79c40bde5f62e
song-dain <inhere31@gmail.com>
2022-10-06 16:04:58 +0900

    송다인 login 기능 수정, header 권한별 메뉴 기능 추가

--- src/main/java/com/greedy/goodeat/common/dto/MemberDTO.java
+++ src/main/java/com/greedy/goodeat/common/dto/MemberDTO.java
@@ -1,8 +1,5 @@
 package com.greedy.goodeat.common.dto;
 
-
-
-
 import lombok.Data;
 
 @Data
--- src/main/java/com/greedy/goodeat/user/service/AuthenticationService.java
+++ src/main/java/com/greedy/goodeat/user/service/AuthenticationService.java
@@ -1,27 +1,36 @@
 package com.greedy.goodeat.user.service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.transaction.Transactional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.greedy.goodeat.common.entity.Member;
 import com.greedy.goodeat.user.repository.MemberRepository;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Transactional
 public class AuthenticationService implements UserDetailsService {
 	
+	private final ModelMapper modelMapper;
 	private final MemberRepository memberRepository;
 	
-	public AuthenticationService(MemberRepository memberRepository) {
+	public AuthenticationService(MemberRepository memberRepository, ModelMapper modelMapper) {
 		this.memberRepository = memberRepository;
+		this.modelMapper = modelMapper;
 	}
 	
 	public Map<String, List<String>> getPermitListMap(){
@@ -36,8 +45,15 @@ public class AuthenticationService implements UserDetailsService {
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Member selectedMember = memberRepository.findByMemberId(username).orElseThrow(() -> new UsernameNotFoundException("회원 정보가 존재하지 않습니다."));
+
+        List<GrantedAuthority> authorites = new ArrayList<>();
+        authorites.add(new SimpleGrantedAuthority(selectedMember.getMemberAuthority().getAuthorityName()));
+        
+        
+        return new User(selectedMember.getMemberId(), selectedMember.getMemberPwd(), authorites);
+
 	}
 
 }
