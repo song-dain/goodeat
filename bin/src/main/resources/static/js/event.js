e99e608f1c952bf7791b9d3f2a3f5414c1b3e570
song-dain <inhere31@gmail.com>
2022-10-11 11:36:11 +0900

    송다인 join 완료

--- bin/src/main/resources/static/js/event.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-	
-	if(document.getElementById("Cart")) {
-		const $Cart = document.getElementById("Cart");
-		$Cart.onclick = function() {
-			location.href = "/user/cartList";
-		}
-	}
-}
